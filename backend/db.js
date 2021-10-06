9f175c3596d249d11c7c6f11fbec4ccc1f653357
Joakim Hannestad Tveter <joakim.tveter@netpower.no>
2021-10-06 21:51:01 +0200

    fixed mobile menu and footer

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const DB = [
+   {
+    productId: 1,
+    title: 'Ping Ping Championship',
+    price: 349,
+    rating: 4.5,
+    platform: ['Playbox', 'Gamestation'],
+    ageRating: '16+',
+    multiplayer: 'Online Multiplayer',
+    grene: ['Action', 'Adventure'],
+    tags: ['Action', 'Adventure', 'Single Player'],
+    relaseDate: '2020-12-15',
+    desciption: '',
+   } 
+]
---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+function openMenu() {
+    const mobileMenu = document.querySelector('#mobile-menu');
+    mobileMenu.classList.add('active');
+}
+
+function closeMenu() {
+    const mobileMenu = document.querySelector('#mobile-menu');
+    mobileMenu.classList.remove('active');
+}
+
+function countdownTimer() {
+    const daysElement = document.querySelector('.timer.days');
+    const hoursElement = document.querySelector('.timer.hrs');
+    const minutesElement = document.querySelector('.timer.min');
+    const secondsElement = document.querySelector('.timer.sec');
+    const target = new Date('2021-10-31T07:00:00').getTime();
+    const now = new Date().getTime();
+    const remaining = target - now;
+  
+    let days = Math.floor(remaining / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+
+    daysElement.innerHTML = (days >= 10) ? days : `0${days}`;
+    hoursElement.innerHTML = (hours >= 10) ? hours : `0${hours}`;
+    minutesElement.innerHTML = (minutes >= 10) ? minutes : `0${minutes}`;;
+    secondsElement.innerHTML = (seconds >= 10) ? seconds : `0${seconds}`;;
+}
+
+setInterval(countdownTimer, 1000);
+
